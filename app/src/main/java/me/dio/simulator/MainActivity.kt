4d37e37deb38befa64c0ece2dfb91737d9f61932
Marilia Vilar <marilia@roadmaps.com.br>
2022-07-17 08:50:47 -0300

    ✨ ➕ feature: Jetpack > View Binding > Habilitando e utilizando

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -2,10 +2,20 @@ package me.dio.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // [VIEWBINDING] - 2. Vincular XML ao .KT através do binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // [VIEWBINDING] - 3. Acessar elementos XML através do binding
+        binding.tvHello.text = "Hello, Android!"
     }
 }
